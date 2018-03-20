cbe604c48ff07f0ca29ca94702bdb51f376c7c3f
superkirill <superkirill@users.noreply.github.com>
2018-03-20 22:01:26 +0700

    First test - Generator initialization

---
+++ main/Generator.py
@@ -0,0 +1,8 @@
+
+
+class Generator():
+    """
+        Generates chords and melodies
+    """
+    def __init__(self):
+        pass
---
+++ test/GeneratorTests.py
@@ -0,0 +1,10 @@
+import unittest
+from main.Generator import *
+
+class MyTestCase(unittest.TestCase):
+    def test_generator_initialization(self):
+        generator = Generator()
+
+
+if __name__ == '__main__':
+    unittest.main()
