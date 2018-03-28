eb542faac68e782f35d971fb869cfd220822d43c
superkirill <sirotkinkirill@yeandex.ru>
2018-03-28 22:50:23 +0700

    Chord generation tests (failed)

--- test/GeneratorTests.py
+++ test/GeneratorTests.py
@@ -9,6 +9,34 @@ class TestInitialization(unittest.TestCase):
     def test_large(self):
         self.failUnlessRaises(NotImplementedError, Generator, 999)
 
+class TestChords(unittest.TestCase):
+    def test_chord_A(self):
+        generator = Generator(0)
+        generator.get_chord('A')
+
+    def test_chord_B(self):
+        generator = Generator(0)
+        generator.get_chord('B')
+
+    def test_chord_C(self):
+        generator = Generator(0)
+        generator.get_chord('C')
+
+    def test_chord_D(self):
+        generator = Generator(0)
+        generator.get_chord('D')
+
+    def test_chord_E(self):
+        generator = Generator(0)
+        generator.get_chord('E')
+
+    def test_chord_F(self):
+        generator = Generator(0)
+        generator.get_chord('F')
+
+    def test_chord_G(self):
+        generator = Generator(0)
+        generator.get_chord('G')
 
 if __name__ == '__main__':
     unittest.main()
