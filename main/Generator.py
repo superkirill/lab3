25c6e896caf64840ebec6b1df43ddee87d6a8867
superkirill <sirotkinkirill@yeandex.ru>
2018-03-28 22:34:02 +0700

    Instrument initialization implementation

--- main/Generator.py
+++ main/Generator.py
@@ -4,5 +4,9 @@ class Generator():
     """
         Generates chords and melodies
     """
-    def __init__(self):
-        pass
+    def __init__(self, instrument=None):
+            pygame.midi.init()
+            player = pygame.midi.Output(0, 0)
+            player.set_instrument(instrument)
+            del player
+            pygame.midi.quit()
